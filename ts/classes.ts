85b8622f7509c184a8c3bfafca9491a4945d5239
kuzhelov <deckster@mail.ru>
2015-11-19 02:35:13 +0300

    add note about lack of support for the 'protected' access specifier

--- ts/classes.ts
+++ ts/classes.ts
@@ -1,5 +1,7 @@
 /* 
 ES6 classes are a simple sugar over the prototype-based OO pattern. Having a single convenient declarative form makes class patterns easier to use, and encourages interoperability. Classes support prototype-based inheritance, super calls, instance and static methods and constructors.
+
+Note that there is no direct support for the protected access specifier.
 */
 
 class Person {
